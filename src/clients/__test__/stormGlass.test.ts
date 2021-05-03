30d0b0428d3926f0a6e26c30a15d9cab51f983ed
Carlos Roberto <crpu4a@gmail.com>
2021-05-03 11:25:55 -0300

    [C02P01-1] - Criando o client stormGlass e seus teste.
    Criação da API, adição e mock do axios e códigos .json para resolver os testes.

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,20 @@
+import { StormGlass } from '@src/clients/stormGlass'
+import axios from 'axios'
+import stormGlassWather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json'
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json'
+
+jest.mock('axios') //  Limpar mocks da dependencia.
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from the StormGlass service', async () => {
+    const lat = -33.792726
+    const lng = 151.289824
+
+    // Fiz um mock para que a função axios.get retorne esse objeto.
+    axios.get = jest.fn().mockResolvedValue(stormGlassWather3HoursFixture) 
+
+    const stormGlass = new StormGlass(axios)
+    const response = await stormGlass.fetchPoints(lat, lng)
+    expect(response).toEqual(stormGlassNormalized3HoursFixture)
+  })
+})
