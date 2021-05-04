8bbe6047965f0042801c02744b8ee2612372e136
Carlos Roberto <crpu4a@gmail.com>
2021-05-04 09:07:21 -0300

    [C02P01-3] - Mock da maneira "certa" com typescript e jest mock.
    Melhora no mock do axios adicionando os tipos do jest.mocked dentro dos tipos do axios. Dessa forma o typescript consegue completar e saber melhor o que está acontecendo.

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,14 +6,15 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normali
 jest.mock('axios') //  Limpar mocks da dependencia.
 
 describe('StormGlass client', () => {
+  // Adicionando os tipos dos jest mocked no axios (somente para testes).
+  const mockedAxios = axios as jest.Mocked<typeof axios>
+
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726
     const lng = 151.289824
-
+    
     // Fiz um mock para que a função axios.get retorne esse objeto.
-    axios.get = jest
-      .fn()
-      .mockResolvedValue({ data: stormGlassWather3HoursFixture })
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWather3HoursFixture })
 
     const stormGlass = new StormGlass(axios)
     const response = await stormGlass.fetchPoints(lat, lng)
