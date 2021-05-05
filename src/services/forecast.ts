0f4daf90c1d9f964665dc08533aace5c9b865de4
Carlos Roberto <crpu4a@gmail.com>
2021-05-05 12:23:23 -0300

    [C03P02] - Forecast Service - Agrupando Forecast por hora.
    Editamos o teste e criamos um método que retorna os dados no novo formato.

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -17,16 +17,22 @@ export interface Beach {
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
+export interface TimeForecast {
+  time: string
+  forecast: BeachForecast[]
+}
+
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
   public async processForecastForBeaches(
     beaches: Beach[]
-  ): Promise<BeachForecast[]> {
+  ): Promise<TimeForecast[]> {
     const pointsWithCorrectSources: BeachForecast[] = []
     for (const beach of beaches) {
       const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng)
       const enrichedBeachData = points.map((e) => ({
+        ...{},
         ...{
           lat: beach.lat,
           lng: beach.lng,
@@ -38,6 +44,22 @@ export class Forecast {
       }))
       pointsWithCorrectSources.push(...enrichedBeachData)
     }
-    return pointsWithCorrectSources
+    return this.mapForecasByTime(pointsWithCorrectSources)
+  }
+
+  private mapForecasByTime(forecast: BeachForecast[]): TimeForecast[] {
+    const forecastByTime: TimeForecast[] = []
+    for (const point of forecast) {
+      const timePoint = forecastByTime.find((f) => f.time == point.time) // caso exista, retorna o primeiro valor.
+      // Caso exista um time
+      if (timePoint) timePoint.forecast.push(point)
+      // caso contrario cria um time
+      else
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point],
+        })
+    }
+    return forecastByTime
   }
 }
