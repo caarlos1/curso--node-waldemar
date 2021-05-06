458a2b1bc807fc85deff23bea549a9f0b734cc7a
Carlos Roberto <crpu4a@gmail.com>
2021-05-06 15:38:11 -0300

    [C04P01] - Configurando mongodb e mongoose.
    Configuração do mongodb e mongoose e edição do jest-setup para finalizar a conexão do mongo depois dos testes.

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,10 +1,13 @@
 import { SetupServer } from '@src/server'
 import supertest from 'supertest'
 
-// Antes de todos os testes da aplicação:
-beforeAll(() => {
-  const server = new SetupServer()
-  server.init() // iniciando o servidor.
+let server: SetupServer
+
+beforeAll(async () => {
+  server = new SetupServer()
+  await server.init() // iniciando o servidor.
   // Setando o servidor de modo global para os testes.
   global.testRequest = supertest(server.getApp())
 })
+
+afterAll(async () => await server.close())
