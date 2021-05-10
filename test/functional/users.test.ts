ab5ef65b11e49d22f36be724d764264875887bc6
Carlos Roberto <crpu4a@gmail.com>
2021-05-10 11:05:54 -0300

    [C05P03] - AuthService, bcrypt e Jest Matchers
    Criação da classe que faz o hash do password.

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,4 +1,6 @@
 import { User } from '@src/models/user'
+import AuthService from '@src/services/auth'
+
 
 describe('User functional tests', () => {
   beforeEach(async () => {
@@ -6,7 +8,7 @@ describe('User functional tests', () => {
   })
 
   describe('When creating a new user', () => {
-    it('should sucessfully create a new user', async () => {
+    it('should sucessfully create a new user with encrypted password', async () => {
       const newUser = {
         name: 'John Doe',
         email: 'john@email.com',
@@ -15,7 +17,15 @@ describe('User functional tests', () => {
 
       const response = await global.testRequest.post('/users').send(newUser)
       expect(response.status).toBe(201)
-      expect(response.body).toEqual(expect.objectContaining(newUser))
+      await expect(
+        AuthService.comparePasswords(newUser.password, response.body.password)
+      ).resolves.toBeTruthy()
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          ...newUser,
+          ...{ password: expect.any(String) },
+        })
+      )
     })
 
     it('should return 422 when there is a validation error', async () => {
@@ -38,16 +48,15 @@ describe('User functional tests', () => {
         email: 'john@email.com',
         password: '1234',
       }
-      
+
       await global.testRequest.post('/users').send(newUser)
       const response = await global.testRequest.post('/users').send(newUser)
 
       expect(response.status).toBe(409)
       expect(response.body).toEqual({
         code: 409,
-        error: 'User validation failed: email: already exists in the database.'
+        error: 'User validation failed: email: already exists in the database.',
       })
-
     })
   })
 })
